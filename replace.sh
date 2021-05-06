bf8077b55646ccf104109ac454115cee36e88e60
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-05-06 13:42:18 +0200

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -5,10 +5,10 @@ echo 'copying files to destination'
 cp -a ${WORKSPACE}/multiscm-pipeline/. ${WORKSPACE}/osszemasolos
 echo 'done'
 echo 'tesztallomany torlese'
-'rm -rf $WORKSPACE/osszemasolos/src/test '
+rm -rf $WORKSPACE/osszemasolos/src/test
 echo 'done'
 echo 'jo tesztallomany beillesztese a projektbe'
-'cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test /var/lib/jenkins/workspace/osszemasolos/src'
+cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test /var/lib/jenkins/workspace/osszemasolos/src
 echo 'done'
-'curl -i localhost:8000'
+curl -i localhost:8000
 echo 'done'
