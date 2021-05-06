52198e2590276e944c70032d08aff0c90152c6be
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-05-06 14:00:40 +0200

    Update AppTest.java

--- src/test/java/com/mkyong/hashing/AppTest.java
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -35,4 +35,20 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    public boolean isEvenNumber(int number){
+         
+        boolean result = false;
+        if(number%2 == 0){
+            result = true;
+        }
+        return result;
+    }
+     
+    @Test
+    public void evenNumberTest(){
+        MyAssertTrueTest asft = new MyAssertTrueTest();
+        assertTrue(asft.isEvenNumber(4));
+    }
+}
 }
