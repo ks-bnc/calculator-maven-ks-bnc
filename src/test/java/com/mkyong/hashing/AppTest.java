405012e4b0e92931d659fe7dc7758878f6992ab5
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-05-06 14:15:32 +0200

    Update AppTest.java

--- src/test/java/com/mkyong/hashing/AppTest.java
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -3,62 +3,36 @@ package com.mkyong.hashing;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
- 
-public class AssertTrueTest {
- 
-    AssertTrueTest att;
-    
-    public boolean isPrime(int number)
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
     {
-        for (int i = 2; i <=Math.sqrt(number); i++) {
-            if(number%i==0)
-                return false;
-        }
-        return true;
+        super( testName );
     }
-    
-    @BeforeEach
-    public void beforeEachTest()
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
     {
-        att = new AssertTrueTest();
+        return new TestSuite( AppTest.class );
     }
-    
-    //assertTrue(boolean condition)
-    @Test
-    public void primeNumberTestwithCondition(){
-        Assertions.assertTrue(att.isPrime(5));
-    }
-    
-    // assertTrue(boolean condition, String message)
-    @Test
-    public void primeNumberTestwithConditionAndMessage(){
-        Assertions.assertTrue(att.isPrime(91),"Number is not prime");
-    }
-    
-    // assertTrue(BooleanSupplier booleanSupplier)
-    @Test
-    public void primeNumberTestWithBooleanSupplier(){
-        Assertions.assertTrue(() -> att.isPrime(7));
-    }
-    
-    //  assertTrue(boolean condition, Supplier<String> messageSupplier)
-    @Test
-    public void primeNumberTestWithConditionAndSupplier(){
-        Assertions.assertTrue(att.isPrime(13),() -> "Number is not prime");
-    }
-    
-    // assertTrue(BooleanSupplier booleanSupplier, String message)
-    @Test
-    public void primeNumberTestWithBooleanSupplierAndMessage(){
-        Assertions.assertTrue(() -> att.isPrime(17),"Number is not prime");
-    }
-    
-    // assertTrue(BooleanSupplier booleanSupplier, Supplier<String> messageSupplier)
-    @Test
-    public void primeNumberTestWithBooleanSupplierAndSupplier(){
-        Assertions.assertTrue(() -> att.isPrime(89),() -> "Number is not prime");
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
