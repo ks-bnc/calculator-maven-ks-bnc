8963f83035be3eebc4620734072b10003d5eb94c
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-05-12 22:30:52 +0200

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,13 +1,10 @@
 echo ${WORKSPACE}
 chmod +x replace.sh
 pwd
-echo 'copying files to destination'
-cp -a ${WORKSPACE}/. /var/lib/jenkins/workspace/osszemasolos
+echo 'delete test source'
+rm -rf ${WORKSPACE}/src/test
 echo 'done'
-echo 'tesztallomany torlese'
-rm -rf /var/lib/jenkins/workspace/osszemasolos/src/test
+echo 'insert the good test source to the project'
+cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test ${WORKSPACE}/src
 echo 'done'
-echo 'jo tesztallomany beillesztese a projektbe'
-cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test /var/lib/jenkins/workspace/osszemasolos/src
-echo 'done'
-echo 'amugy lehet ketto kornyezeti valtozo kellene, egy a first source-nak egy pedig a secondnek ahol ossze masolodik az allomany.. amugy.. pre-build'
+echo 'the test source's replacement successed done'
