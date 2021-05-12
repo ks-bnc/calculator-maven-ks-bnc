fd85ede644f25c5a4902ebe83a0d15545d1c37d5
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-05-12 22:31:56 +0200

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -7,4 +7,4 @@ echo 'done'
 echo 'insert the good test source to the project'
 cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test ${WORKSPACE}/src
 echo 'done'
-echo 'the test source's replacement successed done'
+echo 'the test source's replacement successed'
