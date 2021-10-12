dc555cf24e9e3bcde2b48ae32edd1306e05622d1
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-10-12 21:20:29 +0200

    Update AppTest.java

--- src/test/java/com/mkyong/hashing/AppTest.java
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -4,59 +4,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-private class AssertTrueTest {
-
-    AssertTrueTest att;
-
-    public boolean isPrime(int number)
-    {
-        for (int i = 2; i <=Math.sqrt(number); i++) {
-            if(number%i==0)
-                return false;
-        }
-        return true;
-    }
-
-    @BeforeEach
-    public void beforeEachTest()
-    {
-        att = new AssertTrueTest();
-    }
-
-    //assertTrue(boolean condition)
-    @Test
-    public void primeNumberTestwithCondition(){
-        Assertions.assertTrue(att.isPrime(5));
-    }
-
-    // assertTrue(boolean condition, String message)
-    @Test
-    public void primeNumberTestwithConditionAndMessage(){
-        Assertions.assertTrue(att.isPrime(91),"Number is not prime");
-    }
-
-    // assertTrue(BooleanSupplier booleanSupplier)
-    @Test
-    public void primeNumberTestWithBooleanSupplier(){
-        Assertions.assertTrue(() -> att.isPrime(7));
-    }
-
-    //  assertTrue(boolean condition, Supplier<String> messageSupplier)
-    @Test
-    public void primeNumberTestWithConditionAndSupplier(){
-        Assertions.assertTrue(att.isPrime(13),() -> "Number is not prime");
-    }
-
-    // assertTrue(BooleanSupplier booleanSupplier, String message)
-    @Test
-    public void primeNumberTestWithBooleanSupplierAndMessage(){
-        Assertions.assertTrue(() -> att.isPrime(17),"Number is not prime");
-    }
-
-    // assertTrue(BooleanSupplier booleanSupplier, Supplier<String> messageSupplier)
-    @Test
-    public void primeNumberTestWithBooleanSupplierAndSupplier(){
-        Assertions.assertTrue(() -> att.isPrime(89),() -> "Number is not prime");
-    }
+public class AppTest
+{
+private App app;@Before
+public void setUp() throws Exception {
+app = new App();
+}@After
+public void tearDown() throws Exception {
+}@Test// unit test case that will check whether the method returns the 
+// anticipated output or notpublic void testMultiplication() {
+int result = app.multiply(10, 5);
+assertEquals(result,40);
 }
-
+}
