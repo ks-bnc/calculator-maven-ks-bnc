309b2e307b715599f889e352fb1fd6bec0074e7a
ks-bnc <65093106+ks-bnc@users.noreply.github.com>
2021-10-12 18:58:48 +0200

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -2,9 +2,9 @@ echo ${WORKSPACE}
 chmod +x replace.sh
 pwd
 echo 'delete test source'
-rm -rf ${WORKSPACE}/src/test
+//rm -rf ${WORKSPACE}/src/test
 echo 'done'
 echo 'insert the good test source to the project'
-cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test ${WORKSPACE}/src
+//cp -r /var/lib/jenkins/workspace/multiscm-pipeline-good-test-source/src/test ${WORKSPACE}/src
 echo 'done'
 echo 'the test source replacement successed'
